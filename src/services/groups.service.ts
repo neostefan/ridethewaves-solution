1e6b191c23640eb3384ea478af4f3b08eaaf9e7b
Stephen <stephen.adingupu@gmail.com>
2024-04-06 13:42:35 +0100

    moved to https

--- src/services/groups.service.ts
+++ src/services/groups.service.ts
@@ -3,7 +3,7 @@ import { Group } from "@/columns";
 const BASE_URL =
   import.meta.env.VITE_ENV_MODE === "development"
     ? "http://localhost:4000"
-    : "http://34.105.187.117:4000";
+    : "https://apitest.myftp.biz";
 
 async function getGroupsBySearchTerm(searchTerm: string): Promise<{message: string, data: Group[]} > {
     return (
